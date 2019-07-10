3d5c36ccbb7e29ccb0a7e6babe019a2e3d017773
Tim Rice <timrice1@umbc.edu>
2019-07-10 14:42:14 -0400

    inputAdded

--- proj3.js
+++ proj3.js
@@ -1,1 +1,17 @@
 var x = 0;
+gameIterator = setInterval(iterateGame, 500);
+
+function iterateGame(){
+	console.log("iterate");
+}
+
+function keyDown(){
+	ENTER = 13;
+	keyCode = event.keyCode;
+	
+	if(keyCode == ENTER){
+		console.log(document.getElementById("input").value);
+	}
+}
+
+clearInterval(gameIterator);
