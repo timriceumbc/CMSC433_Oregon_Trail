5fbc37f05f03131618cae4710452391c826034ef
Tim Rice <timrice1@umbc.edu>
2019-07-11 23:29:11 -0400

    update

--- proj3.js
+++ proj3.js
@@ -1,9 +1,44 @@
-var x = 0;
-gameIterator = setInterval(iterateGame, 500);
+var gameIterator = setInterval(iterateGame, 500);
+var playerName = "";
+var occupation = "";
 
+
+class Person{
+	measles = 0;
+	snakebite = 0;
+	exhaustion = 0;
+	typhoid = 0;
+	cholera = 0;
+	dysentary = 0;
+	name = "";
+	
+	constructor(name){
+		this.name = name;
+	}
+}
+
+function textBoxFullscreen(){
+	document.getElementById("textbg").style.top = "8px";
+	document.getElementById("textbg").style.left = "8px";
+	document.getElementById("textbg").style.width = "904px";
+	document.getElementById("textbg").style.height = "586px";
+}
+
+function textBoxInvisible(){
+	document.getElementById("textbg").style.top = "0px";
+	document.getElementById("textbg").style.left = "0px";
+	document.getElementById("textbg").style.width = "0px";
+	document.getElementById("textbg").style.height = "0px";
+}
+
+function startGame(){
+	var p = new Person("h");
+	console.log(p.name);
+}
 
 function iterateGame(){
 	console.log("iterate");
+	textBoxInvisible();
 }
 
 function keyDown(){
@@ -15,4 +50,4 @@ function keyDown(){
 	}
 }
 
-clearInterval(gameIterator);
+window.onload = startGame;
